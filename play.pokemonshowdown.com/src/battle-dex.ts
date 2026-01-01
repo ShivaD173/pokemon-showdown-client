75f86332d48ca457fd313a4b51233b4424adc541
Shiva Devarajan <arcubedgs@gmail.com>
2026-01-01 14:50:21 -0500

    New Stats month

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -1003,7 +1003,7 @@ interface ChaosPokemon {
 }
 
 async function fetchStatsData(format: string): Promise<ChaosResponse> {
-	const url = 'https://staraptorshowdown.com/Stats/2025_9_2025_11/chaos/' + format + '-0.json';
+	const url = 'https://staraptorshowdown.com/Stats/2025_10_2025_12/chaos/' + format + '-0.json';
 	return self.fetch(url).then(res => res.json());
 }
 
